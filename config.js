ad99eff80f6741fec50010167a0ac477e134b6b4
PranavSM-05 <75031003+PranavSM-05@users.noreply.github.com>
2021-08-27 21:55:40 +0530

    Added firebase

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBTENrrUnPjQzdx4QLxeitVNPtdRZOqGLk",
+  authDomain: "votingapp-31c8e.firebaseapp.com",
+  databaseURL: "https://votingapp-31c8e-default-rtdb.firebaseio.com",
+  projectId: "votingapp-31c8e",
+  storageBucket: "votingapp-31c8e.appspot.com",
+  messagingSenderId: "508794278366",
+  appId: "1:508794278366:web:235bc4dea19ed106a28d4f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
