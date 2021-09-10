6a47f732a6adbbe34ad9f2bdb08cc3a40b2ac900
tech-gunk <88415018+tech-gunk@users.noreply.github.com>
2021-09-10 13:41:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./components";
+import Resume from "./components/Resume";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typed from "react-typed";
+import { makeStyles } from "@material-ui/core/styles";
+import avatar from "../avatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(30),
+    height: theme.spacing(30),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "tomato",
+  },
+  subtitle: {
+    color: "tan",
+    textTransform: "uppercase",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.typedContainer}>
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Not found"]} typeSpeed={90} />
+      </Typography>
+
+      
+    </Box>
+  );
+};
+
+export default Header;
