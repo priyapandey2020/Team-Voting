3635b691ebd5f50ca1b9d44d3bacd19bd5b1dc8f
Ani-Whitehat <anirudh.ayyappadas@gmail.com>
2021-09-12 10:18:02 +0530

    changed styles and database

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+// Your web app's Firebase configuration
+    apiKey: "AIzaSyCzsLPbZih-g2XokkZiwLsdWFgli6qK8Ew",
+    authDomain: "classtest-c7a8b.firebaseapp.com",
+    projectId: "classtest-c7a8b",
+    storageBucket: "classtest-c7a8b.appspot.com",
+    messagingSenderId: "662844589879",
+    appId: "1:662844589879:web:68456dc8cfb9e919803cb2"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
