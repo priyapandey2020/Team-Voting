fe48e4fada393df077a2f1b358dddff7162a112e
Prapti28 <75020052+Prapti28@users.noreply.github.com>
2021-09-12 11:08:53 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
-};
+    apiKey: "AIzaSyBU7nseSVr2eSr5swZYCVR2qcqys3x5i8c",
+    authDomain: "team-voting-da9c9.firebaseapp.com",
+    projectId: "team-voting-da9c9",
+    storageBucket: "team-voting-da9c9.appspot.com",
+    messagingSenderId: "1046037497581",
+    appId: "1:1046037497581:web:3ac38b9afa33599b0d2ee4"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
