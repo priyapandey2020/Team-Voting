b79d5ae4a4e0198a6d013f425643cfa21dd23d2f
Vinayak-07 <vinayaknavlekar07@gmail.com>
2021-09-12 10:16:44 +0530

    1

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+    apiKey: "AIzaSyA9jrlaZzDOqARRrReC3U7atImpKIQ3-c8",
+    authDomain: "prop67-d2a68.firebaseapp.com",
+    databaseURL: "https://prop67-d2a68-default-rtdb.firebaseio.com",
+    projectId: "prop67-d2a68",
+    storageBucket: "prop67-d2a68.appspot.com",
+    messagingSenderId: "401333403427",
+    appId: "1:401333403427:web:f5c8a83acd71a1f6f25696"
 };
-// Initialize Firebase
+//// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
