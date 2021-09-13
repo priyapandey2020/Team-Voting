e395408493a1553482e3d9c580973544ae099825
ainesh-tech <64733384+ainesh-tech@users.noreply.github.com>
2021-09-13 17:59:59 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyC2orDYuno4JC00ZKpgj4A5ftj_P-QLbDQ",
+  authDomain: "pro67-ffa93.firebaseapp.com",
+  databaseURL: "https://pro67-ffa93-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pro67-ffa93",
+  storageBucket: "pro67-ffa93.appspot.com",
+  messagingSenderId: "214635859442",
+  appId: "1:214635859442:web:7b5b9f4b181b99c3656f6b"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
