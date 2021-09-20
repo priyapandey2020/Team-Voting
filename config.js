d7348c41b4680f387f3c8cc277405c47261ad03a
gaurikagupta2008 <gaurikagupta17@gmail.com>
2021-09-20 15:41:19 +0530

    add platforms

--- config.js
+++ config.js
@@ -2,9 +2,17 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyCNcg3Ua5fVO8U14_iIgpX-32QaEn4pwVY",
+    authDomain: "voting-app-7f029.firebaseapp.com",
+    databaseURL:"https://voting-app-7f029.firebaseio.com",
+    projectId: "voting-app-7f029",
+    storageBucket: "voting-app-7f029.appspot.com",
+    messagingSenderId: "276864766761",
+    appId: "1:276864766761:web:e41bf6722f39ba30601d38"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
 
 export default firebase.database();
