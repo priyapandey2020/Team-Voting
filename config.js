9ab08ba9576867ecea2a9dcd30ca7e89d1542746
Pranjali Joshi <pranjali06kjoshi@gmail.com>
2021-09-20 08:42:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase';
+ const firebaseConfig = {
+    apiKey: "AIzaSyCxBzLDoLpTcyiuAhrePfRf4RaCxN856Ao",
+    authDomain: "c67-teamvoting.firebaseapp.com",
+    databaseURL: "https://c67-teamvoting-default-rtdb.firebaseio.com",
+    projectId: "c67-teamvoting",
+    storageBucket: "c67-teamvoting.appspot.com",
+    messagingSenderId: "682435700975",
+    appId: "1:682435700975:web:66d910c72f24029e18531a"
+  };
+let app = firebase.initializeApp(firebaseConfig);
+export default app.database();
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase.database();
