23ef39698ba3dbc00bbe56f4fbcdef95b9d11b60
vibhudiwan08 <vibhu08diwan@gmail.com>
2021-09-24 18:57:33 +0530

    Adding firebase

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyD86ksqY9dOVHoN0kH-j6iH61-vceV52cA",
+        authDomain: "buzzerapp-16b98.firebaseapp.com",
+        databaseURL: "https://buzzerapp-16b98-default-rtdb.firebaseio.com",
+        projectId: "buzzerapp-16b98",
+        storageBucket: "buzzerapp-16b98.appspot.com",
+        messagingSenderId: "517791323642",
+        appId: "1:517791323642:web:6bc2c7aa59859d728d4043"
+      
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
