5723984977feb3708940d5963f842da708cc600e
StutieGupta <74605110+StutieGupta@users.noreply.github.com>
2021-09-25 18:06:25 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAecfFprtk2PK-9X2dMUP1L3h_IKx5O94w",
+  authDomain: "team-voting-d3538.firebaseapp.com",
+  databaseURL: "https://team-voting-d3538-default-rtdb.firebaseio.com",
+  projectId: "team-voting-d3538",
+  storageBucket: "team-voting-d3538.appspot.com",
+  messagingSenderId: "396992325071",
+  appId: "1:396992325071:web:73ed35e4c75712f6075eba",
+  measurementId: "G-0GD9FGPTBG",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
