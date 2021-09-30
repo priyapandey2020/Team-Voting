205fe0cf2b346f9845d326c897c05c655a7a2432
DebajitMondal <debashreemondal699@gmail.com>
2021-09-30 13:41:45 +0530

    Firebase Setting

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDDczEn9pJHkY7BlVUGWHvRG1nwl6EVK0Q",
+  authDomain: "project-173bb.firebaseapp.com",
+  databaseURL: "https://project-173bb-default-rtdb.firebaseio.com",
+  projectId: "project-173bb",
+  storageBucket: "project-173bb.appspot.com",
+  messagingSenderId: "1089157994132",
+  appId: "1:1089157994132:web:f4a03e2e79577512b04681"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+let app
+
+if(!firebase.apps.length){
+app = firebase.initializeApp(firebaseConfig)
+
+}
 
 export default firebase.database();
