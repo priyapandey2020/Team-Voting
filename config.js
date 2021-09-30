348dc6080eb060a6e31e638d3142a0226fd2ccc3
Sarang Arora <sarang.kaku11@gmail.com>
2021-09-30 16:12:15 +0530

    'committed'

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyBzJqXjQKg6RNPRKAmdbPbVUY22JGknIyo",
+  authDomain: "project-67-open-source.firebaseapp.com",
+  databaseURL: "https://project-67-open-source-default-rtdb.firebaseio.com",
+  projectId: "project-67-open-source",
+  storageBucket: "project-67-open-source.appspot.com",
+  messagingSenderId: "1093836816539",
+  appId: "1:1093836816539:web:27cd929c108323435e463e"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
