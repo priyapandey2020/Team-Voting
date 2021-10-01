e9212ef8e6a87fc891b8452e9c79dc6433a7e692
PurnimaThapa <76543949+PurnimaThapa@users.noreply.github.com>
2021-10-01 19:46:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+  const firebaseConfig = {
+  apiKey: "AIzaSyAh50laXC2iGnzYn4aWBtTnIHo1BaCVi6s",
+  authDomain: "pro-67-63834.firebaseapp.com",
+  projectId: "pro-67-63834",
+  storageBucket: "pro-67-63834.appspot.com",
+  messagingSenderId: "250263859471",
+  appId: "1:250263859471:web:df4736f40503656c450e7a"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 
 export default firebase.database();
