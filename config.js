668bdca1cf8f47604e1c1c1c048d52d0fe900b00
Netra <netramehta0609@gmail.com>
2021-10-02 18:08:19 +0530

    changed SDK

--- config.js
+++ config.js
@@ -2,23 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-    // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA9qNVtsIZQz3IzXSHg5KzV2MNJTJMJTkY",
+    apiKey: "AIzaSyA9qNVtsIZQz3IzXSHg5KzV2MNJTJMJTkY",
   authDomain: "project-67-fe96f.firebaseapp.com",
   projectId: "project-67-fe96f",
   storageBucket: "project-67-fe96f.appspot.com",
   messagingSenderId: "157723169043",
   appId: "1:157723169043:web:fc2ac16769edfb1a020ae6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
