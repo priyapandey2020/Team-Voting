ed83ab840a195b1e176723fcf2c26d7fddb3ceed
nirmit-cloud <nirmitmodh23@gmail.com>
2021-10-04 20:40:37 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"FFC300",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
