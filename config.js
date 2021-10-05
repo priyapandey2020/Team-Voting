2a985b754aa7177b4ac7861f7f5713dd0a4a923d
aditi-ghadge <74769035+aditi-ghadge@users.noreply.github.com>
2021-10-05 15:32:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDacjX4shUvXh-dm-DN8-EErpUPy92tf6A",
+  authDomain: "project-67-1201c.firebaseapp.com",
+  databaseURL: "https://project-67-1201c-default-rtdb.firebaseio.com",
+  projectId: "project-67-1201c",
+  storageBucket: "project-67-1201c.appspot.com",
+  messagingSenderId: "864061494326",
+  appId: "1:864061494326:web:73921c1740e408dc58b497"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-export default firebase.database();
+export default app.database();
