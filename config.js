bcc2342c32c4ee693c43c6bc94393ee79818c163
Coder-smart574 <73115351+Coder-smart574@users.noreply.github.com>
2021-10-07 17:51:21 +0530

    updated database

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
-
-// add SDK Firebase
-var firebaseConfig = {
-
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyDWR6K9Fb7f-aWFX4dlz5DkaqOaUBhg_2M",
+  authDomain: "voting-team.firebaseapp.com",
+  databaseURL: "https://voting-team-default-rtdb.firebaseio.com",
+  projectId: "voting-team",
+  storageBucket: "voting-team.appspot.com",
+  messagingSenderId: "492802030051",
+  appId: "1:492802030051:web:d3f89de2ca236e0dce88d6"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase.database();
+// Initialize Firebase
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default  firebase.database()
