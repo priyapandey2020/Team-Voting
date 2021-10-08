63ea55a5959cf82ed60486dd287bddbbf7735837
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2021-10-08 18:20:00 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAvnjiUJ6vm4wYfum8TXQAwuDQzSTbb4sg",
+    authDomain: "project-67-3be38.firebaseapp.com",
+    databaseURL: "https://project-67-3be38-default-rtdb.firebaseio.com",
+    projectId: "project-67-3be38",
+    storageBucket: "project-67-3be38.appspot.com",
+    messagingSenderId: "84128876801",
+    appId: "1:84128876801:web:fd50f19ae71e683ddc3300"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
