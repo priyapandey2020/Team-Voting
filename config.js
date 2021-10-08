ed97d881c1bc4ad4692f051d5c3064daa464029d
Shimona012 <bebubabu011@gmail.com>
2021-10-08 19:37:57 +0530

    apps.js

--- config.js
+++ config.js
@@ -2,8 +2,19 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+  apiKey: "AIzaSyCIVUp9YPsH2i2E8LfmVmSnAuJM7976lYI",
+  authDomain: "classtest-8e7ab.firebaseapp.com",
+  projectId: "classtest-8e7ab",
+  storageBucket: "classtest-8e7ab.appspot.com",
+  messagingSenderId: "510973277178",
+  appId: "1:510973277178:web:088b8010415c3db46aaa43"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,17 +6,24 @@ import {
   TouchableOpacity,
   Header,
   Image,
+  Alert
 } from 'react-native';
 
 import AppHeader from '../components/AppHeader';
 import db from '../config';
+import TeamA from './TeamA'
 
 export default class HomeScreen extends Component {
+
+/* goToAScreen=()=> {
+    this.props.navigation.navigate('AScreen');
+  };*/
 
   teamA(){
    db.ref('/').update({
      'teamA':1
    })
+ //  goToAScreen();
  }
 
  teamB(){
@@ -24,18 +31,25 @@ export default class HomeScreen extends Component {
    db.ref('/').update({
      'teamB':2
    })
+  
  }
+Like = () => {
+    alert('Thank you for liking! 😀 ');
+  };
+  Dislike = () => {
+    alert('Thank you for your valuable comment 🙏, I will try to improve it!  ');
+  };
 
   render() {
     return (
-      <View>
+      <View style={{backgroundColor:"cyan"}}>
         <AppHeader />
         <View>
           <View style={styles.buttonsContainer}>
             <TouchableOpacity>
               <Image
-                style={{ width: 300, height: 220, marginLeft: 5 }}
-                source={require('../assets/TeamImage.png')}
+                style={{ width: 300, height: 220, marginLeft: 5,borderColor:'yellow',borderWidth:2 }}
+                source={require('../assets/image-removebg-preview.png')}
               />
             </TouchableOpacity>
           </View>
@@ -54,6 +68,31 @@ export default class HomeScreen extends Component {
             </TouchableOpacity>
 
           </View>
+        </View>
+        <View style={{backgroundColor:"cyan"}}>
+        <TouchableOpacity onPress={this.Like}>
+          <Image
+            source={require('../assets/thumbs-up-hand-symbol.png')}
+            style={{ width: 60, height: 60, margin:10 }}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.Dislike}>
+          <Image
+            source={require('../assets/thumbs-down-silhouette.png')}
+            style={{ width: 60, height: 60, marginLeft: 250, marginTop:-60,marginBottom:24 }}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            marginLeft: 140,
+            marginTop: -40,
+            fontFamily: 'chiller regular',
+            fontSize: 25,
+            // fontWeight:'bold',
+          }}>
+          Rate Us!
+        </Text>
         </View>
       </View>
     );
