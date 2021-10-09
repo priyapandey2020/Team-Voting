437fac010ac0c9fd112b40434229141bdf3701b5
Omar Othman <omaroth24@gmail.com>
2021-10-09 17:49:40 -0400

    I committed the changes

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+var firebaseConfig = { 
+        apiKey: "AIzaSyBQz84Qkz0ESG7JEt14-QK7uwpt68aF8D4",
+        authDomain: "classtest-affeb.firebaseapp.com",
+        projectId: "classtest-affeb",
+        storageBucket: "classtest-affeb.appspot.com",
+        messagingSenderId: "553574070648",
+        appId: "1:553574070648:web:433994fc71e259bfd1f08d"
 
 };
 // Initialize Firebase
