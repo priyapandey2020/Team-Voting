9f74d6b0c146b328723105c5423ae74f7dadd8c4
mishrasukirti9 <mishrasukirti9@gmail.com>
2021-10-10 16:44:50 +0530

    Created database

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = { apiKey: "AIzaSyD91pzYOu9qMFEFiwwori-FzBSI2_U_dAA", 
+authDomain: "classtest-4be03.firebaseapp.com",
+ projectId: "classtest-4be03", 
+ storageBucket: "classtest-4be03.appspot.com", 
+ messagingSenderId: "677954962728", 
+ appId: "1:677954962728:web:92b8d761cb60d566c63c1d",
+  measurementId: "G-1K49TPFY0N" 
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
