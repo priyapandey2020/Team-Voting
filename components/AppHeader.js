d8be1882dbe0fffc889f46f5a28be6e9c86b8f5b
Zuha Fathima <72133440+ZuhaFathima@users.noreply.github.com>
2021-10-12 12:56:53 +0530

    Update AppHeader.js

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,7 +13,7 @@ export default class AppHeader extends React.Component{
 
 const styles = StyleSheet.create({
   textContainer:{
-    backgroundColor: '#FFC300'
+    backgroundColor: 'Blue'
   },
   text:{
     padding: 20,
