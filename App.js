cd5bf72606e1858aa1e61c106ae914aa548bb28f
ruchith527 <82458071+ruchith527@users.noreply.github.com>
2021-10-13 17:40:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 
 // You can import from local files
 import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 
 export default class App extends React.Component {
   render() {
