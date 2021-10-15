d8a8dfb3211536d90821863bc6f46b81ddc5d1b7
Purva-Patil05 <purvapatil986@gmail.com>
2021-10-15 14:24:11 +0530

    made a ew firebase

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
-import firebase from 'firebase';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfBkKh7wASK-oh_oZXcsULBzvvRlwiOKg",
+  authDomain: "team-voting-a62da.firebaseapp.com",
+  projectId: "team-voting-a62da",
+  storageBucket: "team-voting-a62da.appspot.com",
+  messagingSenderId: "23887983680",
+  appId: "1:23887983680:web:1eafb3dea4ce5a21431ba5"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
