abc0b385c08826ff31db6d649eae29bd96b0ec35
ugos520 <timxu520@gmail.com>
2021-10-17 22:50:29 -0400

    add changes

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC8pq2pbhUL5lvSad4awjXUTWNw6esjs2g",
+    authDomain: "project67-946ed.firebaseapp.com",
+    projectId: "project67-946ed",
+    storageBucket: "project67-946ed.appspot.com",
+    messagingSenderId: "110684383828",
+    appId: "1:110684383828:web:54cb982fd7b77e03211bc1"
+  };
+
 var firebaseConfig = {
 
 };
-// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
