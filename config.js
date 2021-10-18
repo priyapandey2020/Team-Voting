355c13e0b3eb55071b64f8c0005363d04eafd397
voscrolp <70542514+voscrolp@users.noreply.github.com>
2021-10-18 15:03:52 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyCr-ISlSs_daUhhcYqjugN4znLUzjEUr2g",
+        authDomain: "eeee-d2d43.firebaseapp.com",
+        databaseURL: "https://eeee-d2d43-default-rtdb.firebaseio.com",
+        projectId: "eeee-d2d43",
+        storageBucket: "eeee-d2d43.appspot.com",
+        messagingSenderId: "905350950633",
+        appId: "1:905350950633:web:711a4f8a03f29676434a78"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
