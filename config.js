66ba9bac5effca93a48cb44426da0097ff99d82e
DishikaAggarwal <dishikaagg@gmail.com>
2021-10-20 18:56:06 +0530

    trying...

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyB4sCRH9XtzgVylWijif570l01qzn3f9Cc",
+    authDomain: "team-voting-50bec.firebaseapp.com",
+    databaseURL: "https://team-voting-50bec-default-rtdb.firebaseio.com",
+    projectId: "team-voting-50bec",
+    storageBucket: "team-voting-50bec.appspot.com",
+    messagingSenderId: "1060487203043",
+    appId: "1:1060487203043:web:80b8886200ea026f03ddd6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
