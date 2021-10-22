0e144728d9554892fcb3b4fb3f688c27079361e1
Utkarsh <utkarsh3007chaturvedi@gmail.com>
2021-10-22 16:58:28 +0530

    updated

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC1Owu0yWEGS7WmiaOZRqmDd3xSpTWi70Q",
+    authDomain: "voting-app-9ba73.firebaseapp.com",
+    databaseURL: "https://voting-app-9ba73-default-rtdb.firebaseio.com",
+    projectId: "voting-app-9ba73",
+    storageBucket: "voting-app-9ba73.appspot.com",
+    messagingSenderId: "568458097042",
+    appId: "1:568458097042:web:dd2829c148f0f49e00d14b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 export default firebase.database();
