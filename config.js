e8eaae7d7c1d0502251916c32073315ff3a332c3
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2021-10-24 00:26:24 +0530

    adding firebase

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA0-3Yr33OvM9ZPZbs7bGdOHye0Ek-ucqU",
+  authDomain: "pro67-e6609.firebaseapp.com",
+  projectId: "pro67-e6609",
+  storageBucket: "pro67-e6609.appspot.com",
+  messagingSenderId: "960865846464",
+  appId: "1:960865846464:web:2778545df7e635c64ed925"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
