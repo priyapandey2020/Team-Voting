24a22e2203a9386fb579892d5d5fa15bb1715cab
sanskar <sahusanskar1007@gmail.com>
2021-10-27 22:45:44 +0530

    added SDK

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase';
-
-// add SDK Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB7utsOLjAry0aBEGVj3SPKOFSh5hUaEJg",
+    authDomain: "team-voting-app-b6a3f.firebaseapp.com",
+    databaseURL: "https://team-voting-app-b6a3f-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-b6a3f",
+    storageBucket: "team-voting-app-b6a3f.appspot.com",
+    messagingSenderId: "71247718554",
+    appId: "1:71247718554:web:389e55ec9832aabffb5723"
+  };
 var firebaseConfig = {
 
 };
