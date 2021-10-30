d6e9d34a12e46918cd1883eb33a2c36a66dfeb1b
GhostRiderAdb <72968207+GhostRiderAdb@users.noreply.github.com>
2021-10-30 10:54:01 +0530

    Update config.js
    added database

--- config.js
+++ config.js
@@ -2,7 +2,17 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyDS4JrphHfna3q5gYkbmx8reBjOJWPXfI0",
+  authDomain: "vote-app-5cdaf.firebaseapp.com",
+  projectId: "vote-app-5cdaf",
+  storageBucket: "vote-app-5cdaf.appspot.com",
+  messagingSenderId: "45510156867",
+  appId: "1:45510156867:web:d0e47aabc883198677eaf6"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
