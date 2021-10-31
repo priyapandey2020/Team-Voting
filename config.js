e28f44050e2e958da949cc95af141d0a5a2d9301
Utkarsh <utkarshsaha369@gmail.com>
2021-10-31 11:34:26 +0530

    changed some styles

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyBdjO-N6rNqXSwUxAX_6N0ZfAOnA3WIMtc",
+    authDomain: "pro-67-ed00b.firebaseapp.com",
+    databaseURL: "https://pro-67-ed00b-default-rtdb.firebaseio.com",
+    projectId: "pro-67-ed00b",
+    storageBucket: "pro-67-ed00b.appspot.com",
+    messagingSenderId: "593844302601",
+    appId: "1:593844302601:web:dee098639924ca05a3d9d8"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
