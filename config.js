9d13d9d299136d4b331c75218dd1cb87b1ef8d4d
PalaashBatra <78481121+PalaashBatra@users.noreply.github.com>
2021-11-03 19:47:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyDxMS0xB4GzDAqRuL62Ygdkk8UgJY0Pqtg",
+  authDomain: "teamvoting-5fdf2.firebaseapp.com",
+  projectId: "teamvoting-5fdf2",
+  storageBucket: "teamvoting-5fdf2.appspot.com",
+  messagingSenderId: "302994912393",
+  appId: "1:302994912393:web:b8afdf71d8f356ecce0258"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
