da99a09e098fec1c0888a9162f4b5dbe21e80ad0
tokukita <75066951+Nebro71011@users.noreply.github.com>
2021-11-06 11:55:50 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"aquamarine",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
