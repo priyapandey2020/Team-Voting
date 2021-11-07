af7683b7c87462bb680cf7b9e612edeafe67e219
Ruban <rubanclare15@gmail.com>
2021-11-07 10:56:37 -0700

    added firebase config

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyA59PhXr_a128sFD2LOOVHym8g9_tnfFsk",
+    authDomain: "votingapp-7aa89.firebaseapp.com",
+    projectId: "votingapp-7aa89",
+    storageBucket: "votingapp-7aa89.appspot.com",
+    messagingSenderId: "1037353074332",
+    appId: "1:1037353074332:web:a73d98ef927ade42b43759"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
