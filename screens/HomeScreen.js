25bcf4722ff53ccec1f339305fbcb970e30c378b
SamTheGreat <74596262+SaM11012008@users.noreply.github.com>
2021-11-11 11:25:53 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"#a2a4ab",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
