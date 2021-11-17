c7309e9e71e8288ac09897cd5a9b9e4d8e6388e0
Riddhiman Sharma <itsridsweetu2007@gmail.com>
2021-11-17 11:36:39 +0530

    added firebase

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyAIXpN1Ge1Bu_g62weMKc6WxN_0uPsHqcs",
+        authDomain: "team-voting-5f2b5.firebaseapp.com",
+        projectId: "team-voting-5f2b5",
+        storageBucket: "team-voting-5f2b5.appspot.com",
+        messagingSenderId: "704862997279",
+        appId: "1:704862997279:web:4784d1e9835372f636fe2a"
+      
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
