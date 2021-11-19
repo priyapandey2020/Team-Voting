445d07491d0a79d90207ca5318d2c25acfd5c9fb
Garvita <garvitajeon@gmail.com>
2021-11-19 22:41:27 +0530

    Removedthe rounded edges of the buttons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -70,7 +70,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
-    borderRadius: 15,
     margin:10,
     width: 150,
     height: 50,
