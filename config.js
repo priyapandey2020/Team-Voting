f146165da2949d2f7e1486ca3ddee9ad43caeefc
awesomegamer9595 <awesomegamer9595@gmail.com>
2021-11-28 21:02:47 -0500

    UI CHANGES

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAkiJ7McZONQmtpgFsa_ZiluHE1cpNL92U",
+    authDomain: "teamvotingapp-e41e9.firebaseapp.com",
+    projectId: "teamvotingapp-e41e9",
+    storageBucket: "teamvotingapp-e41e9.appspot.com",
+    messagingSenderId: "134750976376",
+    appId: "1:134750976376:web:bbe5f93d7ec6fa596c308d"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
