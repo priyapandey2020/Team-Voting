d0337fb753d3942ba5c0d99e83e6c76e4838c5e7
hraj26058 <81627508+hraj26058@users.noreply.github.com>
2021-11-30 10:05:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDy-ASUItqYDZT2AZIIz3I1lBTSyPnC3zI",
+    authDomain: "classtest-c56f0.firebaseapp.com",
+    projectId: "classtest-c56f0",
+    storageBucket: "classtest-c56f0.appspot.com",
+    messagingSenderId: "299285221454",
+    appId: "1:299285221454:web:3fd924a8aadf8aa21df31f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
