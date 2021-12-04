fae0e94e8282fcaa490930b4f24dc9422458d72a
Nitin Gopal <nitingopalrajagopal@gmail.com>
2021-12-04 09:46:05 +0530

    Changed Firebase and Changed Styling

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyCOyHSiFWb9w5GQgYOcAQWY9uAtWeEOCKk",
+    authDomain: "c67-project-59621.firebaseapp.com",
+    projectId: "c67-project-59621",
+    storageBucket: "c67-project-59621.appspot.com",
+    messagingSenderId: "873764428538",
+    appId: "1:873764428538:web:5a196c58d153955f604743"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
