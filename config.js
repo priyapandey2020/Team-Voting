3ea52328eb8e5ea45cf4268f0c4e7f4b4dae5176
jerrinjosewhitehat <83947817+jerrinjosewhitehat@users.noreply.github.com>
2021-12-05 16:44:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBBrTdfea0rBpBhZ7P5NySdN642pXdxjQM",
+  authDomain: "team-voting-app-b7ec6.firebaseapp.com",
+  projectId: "team-voting-app-b7ec6",
+  storageBucket: "team-voting-app-b7ec6.appspot.com",
+  messagingSenderId: "23347487768",
+  appId: "1:23347487768:web:dd04032ec04d458fc38f6d"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
