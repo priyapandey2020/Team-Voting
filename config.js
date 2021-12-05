5898bec6b2484c798ce463dd9f8bcbc37a72ebef
shivanshu-pattnaik <74895692+ShivanshuPattnaik@users.noreply.github.com>
2021-12-05 20:29:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBSBDF6Vgt-5cFVHUxQ0haUuiwrwmnv4Tk",
+    authDomain: "team-voting-595b5.firebaseapp.com",
+    databaseURL: "https://team-voting-595b5-default-rtdb.firebaseio.com",
+    projectId: "team-voting-595b5",
+    storageBucket: "team-voting-595b5.appspot.com",
+    messagingSenderId: "476818606390",
+    appId: "1:476818606390:web:565b384f853e20b55a0b97"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
