aa98beed3187efe9122f5c7b455eed70ae8adcf4
deepthina <deepthi.an.87@gmail.com>
2021-12-20 17:25:51 +0530

    Added config

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyB66GU5XsV338BATZk8Jys8riBO41AcZ_I",
+  authDomain: "p58--team-voting-app.firebaseapp.com",
+  databaseURL: "https://p58--team-voting-app-default-rtdb.firebaseio.com",
+  projectId: "p58--team-voting-app",
+  storageBucket: "p58--team-voting-app.appspot.com",
+  messagingSenderId: "998784628007",
+  appId: "1:998784628007:web:78cab1129274d2430ea502",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
