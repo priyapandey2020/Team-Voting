f60ac77f68dd7c827e9c82c8cd658f0abff693a0
m3v113 <mehvishbondre@gmail.com>
2021-12-22 14:17:47 -0800

    connected databse

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyCqCRWtHX--wLJQzgrK96B6u7PhYdXn6Ig",
+    authDomain: "team-voting-f6c29.firebaseapp.com",
+    projectId: "team-voting-f6c29",
+    storageBucket: "team-voting-f6c29.appspot.com",
+    messagingSenderId: "687870187166",
+    appId: "1:687870187166:web:ed5e564ae66e494224cd8f",
+    measurementId: "G-WY8KZKX32R"
 
 };
 // Initialize Firebase
