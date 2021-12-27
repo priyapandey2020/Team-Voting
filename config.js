3518d486a48605603b244648ef1be260caa895d7
HYDRAISHI <82408105+HYDRAISHI@users.noreply.github.com>
2021-12-27 17:43:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyA2ARDNnYRpjy7G6_HoN21AZYlCK07b2HU",
+  authDomain: "team-voting-app-d5a30.firebaseapp.com",
+  projectId: "team-voting-app-d5a30",
+  storageBucket: "team-voting-app-d5a30.appspot.com",
+  messagingSenderId: "752508181346",
+  appId: "1:752508181346:web:7b7cc64e6b20d81741aa3b"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
