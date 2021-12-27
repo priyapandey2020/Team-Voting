b36c3f75fbe9e28f14a888370a997bc7c6286972
jayantyadav001 <082005jayant@gmail.com>
2021-12-27 18:36:50 +0530

    added configuratrion

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB2uklnhHOszvqSCLlO2k-o87_wq3Wa2OE",
+    authDomain: "class-58jno.firebaseapp.com",
+    databaseURL: "https://class-58jno-default-rtdb.firebaseio.com",
+    projectId: "class-58jno",
+    storageBucket: "class-58jno.appspot.com",
+    messagingSenderId: "528837858091",
+    appId: "1:528837858091:web:afcc75095ecefac88c2b6e"
+  };
 // Initialize Firebase
+if(!firebase.apps.length)
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
