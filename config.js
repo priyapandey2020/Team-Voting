ccafcb36c73da33d8cc240eeedbeafc1f5d8668e
Vinita Gangraj <vinitagangraj@gmail.com>
2021-12-28 16:45:39 +0530

    Add the config and homescreen changes

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAIKFKcZCoxCP58v0V4rPjPx1iSi8xDItU",
+    authDomain: "my-quiz-game-32a91.firebaseapp.com",
+    databaseURL: "https://my-quiz-game-32a91-default-rtdb.firebaseio.com",
+    projectId: "my-quiz-game-32a91",
+    storageBucket: "my-quiz-game-32a91.appspot.com",
+    messagingSenderId: "634847826731",
+    appId: "1:634847826731:web:df6aed0bf64e98ffd56b6e"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
