cc7f32cd3ee1714e2a27a05c9f032bb6557abd1c
Amish-sherlekar <asmitasherlekar@gmail.com>
2021-12-30 11:31:51 +0530

    myCommit1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Text,
   View,
@@ -6,25 +6,24 @@ import {
   TouchableOpacity,
   Header,
   Image,
-} from 'react-native';
+} from "react-native";
 
-import AppHeader from '../components/AppHeader';
-import db from '../config';
+import AppHeader from "../components/AppHeader";
+import db from "../config";
 
 export default class HomeScreen extends Component {
+  teamA() {
+    db.ref("/").update({
+      teamA: 1,
+    });
+  }
 
-  teamA(){
-   db.ref('/').update({
-     'teamA':1
-   })
- }
-
- teamB(){
-   console.log(db);
-   db.ref('/').update({
-     'teamB':2
-   })
- }
+  teamB() {
+    console.log(db);
+    db.ref("/").update({
+      teamB: 2,
+    });
+  }
 
   render() {
     return (
@@ -35,24 +34,29 @@ export default class HomeScreen extends Component {
             <TouchableOpacity>
               <Image
                 style={{ width: 300, height: 220, marginLeft: 5 }}
-                source={require('../assets/TeamImage.png')}
+                source={require("../assets/TeamImage.png")}
               />
             </TouchableOpacity>
           </View>
           <View style={styles.ratingContainer}>
-            <Text style={{ textAlign: 'center',fontSize:25 }}>Vote Here</Text>
+            <Text style={{ textAlign: "center", fontSize: 25 }}>Vote Here</Text>
             <TouchableOpacity
               style={styles.buttons}
-              onPress ={this.teamA()}>
-              <Text style={{ fontSize:20}}>Team A</Text>
+              onPress={() => {
+                this.teamA();
+              }}
+            >
+              <Text style={{ fontSize: 20 }}>Team A</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
               style={styles.buttons}
-              onPress ={this.teamB()}>
-              <Text style={{ fontSize:20}}>Team B</Text>
+              onPress={() => {
+                this.teamB();
+              }}
+            >
+              <Text style={{ fontSize: 20 }}>Team B</Text>
             </TouchableOpacity>
-
           </View>
         </View>
       </View>
@@ -62,21 +66,21 @@ export default class HomeScreen extends Component {
 
 const styles = StyleSheet.create({
   buttonsContainer: {
-    alignSelf: 'center',
+    alignSelf: "center",
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "coral",
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 1,
     borderRadius: 15,
-    margin:10,
+    margin: 10,
     width: 150,
     height: 50,
   },
   ratingContainer: {
-    alignSelf: 'center',
+    alignSelf: "center",
     marginTop: 50,
   },
 });
