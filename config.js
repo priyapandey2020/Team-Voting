ce58f67c99e13ba2acd2f821cf07d350e9f7c840
Samriddhi Sarawgi <samriddhisarawgi@gmail.com>
2022-01-02 15:14:56 +0530

    first commit - styling the button

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+    
+  apiKey: "AIzaSyCZ4MbL1t34rpdKBknRFhvyDPhHWT-8IoE",
+  authDomain: "classtest-44039.firebaseapp.com",
+  projectId: "classtest-44039",
+  storageBucket: "classtest-44039.appspot.com",
+  messagingSenderId: "832773116722",
+  appId: "1:832773116722:web:86553e92893e684ad0bd2b"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
