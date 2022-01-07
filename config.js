89c442301bb4dee5cee2207f4cabaf989ff9b0ae
Rithik Sujith <rithiksujith77@gmail.com>
2022-01-07 17:43:44 +0530

    changed bg color

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyBdJcNNUDPFbNV1JP00IkVQ8blxFEZ2jFM",
+    authDomain: "c67-pro.firebaseapp.com",
+    projectId: "c67-pro",
+    storageBucket: "c67-pro.appspot.com",
+    messagingSenderId: "414385723810",
+    appId: "1:414385723810:web:3727f19a72899eabcd7afb",
+    measurementId: "G-DPPKPWJ8VM"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
