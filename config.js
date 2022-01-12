7a0e3967a86abc3e178306bdd0827d1f59b53138
maaz44hello <79090862+maaz44hello@users.noreply.github.com>
2022-01-12 19:19:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-const firebaseConfig = {
+
   apiKey: "AIzaSyDBXAq4rMmw_Y_4xdUikMZvhq_PfA3deBI",
   authDomain: "voting-app-265d4.firebaseapp.com",
   databaseURL: "https://voting-app-265d4-default-rtdb.firebaseio.com",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "499501172413",
   appId: "1:499501172413:web:fda5e56b5cea31ea6259d4"
 };
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
