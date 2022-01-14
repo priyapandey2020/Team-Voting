06269594f44ba404f8b22483e8dc4b6e10900936
goru155 <74966689+goru155@users.noreply.github.com>
2022-01-14 14:17:26 +0530

    database added

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyDzhYvlFdpC47bOxks0hg70bACMSPHJYqE",
+        authDomain: "voting-app-6b2f8.firebaseapp.com",
+        databaseURL: "https://voting-app-6b2f8-default-rtdb.firebaseio.com",
+        projectId: "voting-app-6b2f8",
+        storageBucket: "voting-app-6b2f8.appspot.com",
+        messagingSenderId: "209423201637",
+        appId: "1:209423201637:web:1eca8621bf5096ed189d8c"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
