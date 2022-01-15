f0f8cdfd8b89f35e7c28b22fabd1628440c09c54
BFSmx6 <97541827+BFSmx6@users.noreply.github.com>
2022-01-15 12:00:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
+// Agregar SDK Firebase
 var firebaseConfig = {
 
 };
-// Initialize Firebase
+// Izicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
