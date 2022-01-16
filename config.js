0516cbff806cd45415358566281c129352d5e436
SpeedGod14 <tooniloba@gmail.com>
2022-01-16 13:46:47 -0500

    add configurations

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB0YeRRGcOezxHXGQPszo8GODmlSmXdJ78",
+    authDomain: "team-voting-23b4c.firebaseapp.com",
+    databaseURL: "https://team-voting-23b4c-default-rtdb.firebaseio.com",
+    projectId: "team-voting-23b4c",
+    storageBucket: "team-voting-23b4c.appspot.com",
+    messagingSenderId: "864777483199",
+    appId: "1:864777483199:web:db55aca9769b66e4e9670a"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
