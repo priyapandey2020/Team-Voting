3321871504bd210d20719c6079af9a29c81484a7
pragnakadu <71829814+pragnakadu@users.noreply.github.com>
2022-01-17 10:56:53 +0530

    added the sdk kit for firebase

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+ apiKey: "AIzaSyBuIwYyP5NkA_ZDFZjAa_eqASOLxZPK6xw",
+  authDomain: "pro58teamvotingappwhjr.firebaseapp.com",
+  projectId: "pro58teamvotingappwhjr",
+  storageBucket: "pro58teamvotingappwhjr.appspot.com",
+  messagingSenderId: "268883680240",
+  appId: "1:268883680240:web:f01e7d707ddb537bb39b35"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
