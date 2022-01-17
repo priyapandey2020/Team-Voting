4150f7ab448e71c84c02dea8ed4193f61ec2fecc
Swagatika244 <swagatikamohapatra244@gmail.com>
2022-01-17 20:56:48 +0530

    Change heading and button color

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,11 +13,13 @@ export default class AppHeader extends React.Component{
 
 const styles = StyleSheet.create({
   textContainer:{
-    backgroundColor: '#FFC300'
+    backgroundColor: 'aqua',
+    borderBottomWidth: 5
+
   },
   text:{
     padding: 20,
-    fontSize: 22,
+    fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
   }
