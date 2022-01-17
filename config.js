f4b6a47c3824541604694dcd51baf0d0c31d857b
luckylovs <lalit.info.in@gmail.com>
2022-01-17 11:24:11 +0530

    Changing FontSize

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyCp1RDSL0xbyYYTY3Ua_-Foh5PaRa1Bk7I",
+        authDomain: "teamvotingapp-2a84d.firebaseapp.com",
+        databaseURL: "https://teamvotingapp-2a84d-default-rtdb.firebaseio.com",
+        projectId: "teamvotingapp-2a84d",
+        storageBucket: "teamvotingapp-2a84d.appspot.com",
+        messagingSenderId: "337878392939",
+        appId: "1:337878392939:web:6fa9e0596f83dca285912e"
+      };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
