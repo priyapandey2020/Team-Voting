4781db940948a11f98a1348dae7c341528a8efb6
adityamukhi <83415279+adityamukhi@users.noreply.github.com>
2022-01-19 18:46:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGQ-yIEbvM0XWVeOa9cGjNR4Pfvt2i-IA",
+  authDomain: "votingapp-2469e.firebaseapp.com",
+  projectId: "votingapp-2469e",
+  storageBucket: "votingapp-2469e.appspot.com",
+  messagingSenderId: "1052513940722",
+  appId: "1:1052513940722:web:daa3c7fb8f0fc36d12cc6b",
+  measurementId: "G-9GL12HHSE1"
+};
 var firebaseConfig = {
 
 };
