9bfdc7eeba1b25dfbbf5ffbd332bdcacd8e3aeb1
yuvika <nkkagrawals@gmail.com>
2022-01-20 14:11:10 +0530

    done the changes

--- config.js
+++ config.js
@@ -6,5 +6,16 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDCKDQhsY1doDgDhjCR582bpXQBrZGw62Q",
+    authDomain: "project67-57014.firebaseapp.com",
+    databaseURL: "https://project67-57014-default-rtdb.firebaseio.com",
+    projectId: "project67-57014",
+    storageBucket: "project67-57014.appspot.com",
+    messagingSenderId: "173354418575",
+    appId: "1:173354418575:web:78535eb1e4e70942b74bfa"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 export default firebase.database();
