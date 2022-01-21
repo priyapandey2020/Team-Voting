22e5fe16785cfb6ff5d6700e9fd2d39c1722c874
raghav517 <raghav.gajpure@gmail.com>
2022-01-21 21:44:49 +0530

    Changes Done

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB4bxzUQIZatiD8TG_EJYTNKwenlIpfXK0",
+    authDomain: "pro-c67-cfd27.firebaseapp.com",
+    projectId: "pro-c67-cfd27",
+    storageBucket: "pro-c67-cfd27.appspot.com",
+    messagingSenderId: "337692074075",
+    appId: "1:337692074075:web:fc813c313e1cccb5e21f7e",
+    measurementId: "G-2E63RRWJ4X"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
