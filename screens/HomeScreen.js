88d9cb63eda0f8dd32e1f9b571898cfd647b20b1
Koolkan0709 <atharvankanav@gmail.com>
2022-01-25 16:22:01 +0530

    changing button color to blue

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"cyan",
+    backgroundColor:"blue",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 2,
