795ca95f8f635f25ba85dab22364222baf721879
Smriti-coder <79551556+Smriti-coder@users.noreply.github.com>
2022-01-29 17:47:00 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
-
-// add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUYouHgFEHa82nowkvU9Kf8E1fUe5Xh1Y",
+  authDomain: "voting-app-73951.firebaseapp.com",
+  databaseURL: "https://voting-app-73951-default-rtdb.firebaseio.com",
+  projectId: "voting-app-73951",
+  storageBucket: "voting-app-73951.appspot.com",
+  messagingSenderId: "843491398855",
+  appId: "1:843491398855:web:f3b3cb7c68716710991ae2"
 };
+
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
