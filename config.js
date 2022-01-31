4ced66f7ef336cb87131acd93db9401e39b6ed85
Manasvi2402 <74135076+Manasvi2402@users.noreply.github.com>
2022-01-31 15:48:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
+  const firebaseConfig = {
+  apiKey: "AIzaSyCn6sMmd5jUtn58gX73fpAsu5BCdRFzHIo",
+  authDomain: "team-voting-app-49a1a.firebaseapp.com",
+  projectId: "team-voting-app-49a1a",
+  storageBucket: "team-voting-app-49a1a.appspot.com",
+  messagingSenderId: "261758033430",
+  appId: "1:261758033430:web:7bb72ecca2150d59065f37"
 };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
