cb826620a62cb38705edfeb253309980d310bed7
Pratyush <pratyushraj0176@gmail.com>
2022-01-31 16:54:43 +0530

    changes

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqNpIzjvaW7NynCDMAKwdshHvzJ63uVN0",
+  authDomain: "p67-team-voting.firebaseapp.com",
+  projectId: "p67-team-voting",
+  storageBucket: "p67-team-voting.appspot.com",
+  messagingSenderId: "91431129854",
+  appId: "1:91431129854:web:586aa030eb0c73acffcbaf"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
