e7908c9ad6ec1119ee83b3e616d2c83389a1a571
azan788 <72837805+azan788@users.noreply.github.com>
2022-02-06 13:01:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRq3VnwyRQbmx3-W-JM185V9uaYbo_rlo',
+  authDomain: 'project-67-87138.firebaseapp.com',
+  databaseURL: 'https://project-67-87138-default-rtdb.firebaseio.com',
+  projectId: 'project-67-87138',
+  storageBucket: 'project-67-87138.appspot.com',
+  messagingSenderId: '119493850750',
+  appId: '1:119493850750:web:7e2c67a09311c818b50a2f',
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 export default firebase.database();
