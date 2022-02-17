8f80a11082973f2b3b5812e060386bff3e76223d
TTVTrob <82639775+TTVTrob@users.noreply.github.com>
2022-02-17 17:40:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDXK9z9FRcLfYqcUwHScFIBKPUnPAEud7o",
+  authDomain: "voters-117f5.firebaseapp.com",
+  databaseURL: "https://voters-117f5-default-rtdb.firebaseio.com",
+  projectId: "voters-117f5",
+  storageBucket: "voters-117f5.appspot.com",
+  messagingSenderId: "404093287980",
+  appId: "1:404093287980:web:a3e6d3ec8a676c0c1a9b89"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
