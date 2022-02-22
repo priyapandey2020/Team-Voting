cc663ac7c63f15b658e535b7e6887cf07b8ee9ae
invicible-om <abhaykritsingh@gmail.com>
2022-02-22 08:07:37 +0530

    hello

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"white",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
