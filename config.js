f7a6656c77d604b23af83d4582c02e7700d049a2
invicible-om <abhaykritsingh@gmail.com>
2022-02-22 08:05:16 +0530

    hello

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAo5DQFdLUljcGAIHbZzIl-08K-oTjWtbI",
+  authDomain: "pro-67--team-voting.firebaseapp.com",
+  projectId: "pro-67--team-voting",
+  storageBucket: "pro-67--team-voting.appspot.com",
+  messagingSenderId: "1042130991628",
+  appId: "1:1042130991628:web:59819bdb8b5cef03738c29",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
