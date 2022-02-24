fcd6144c125d30692c680a5e0df0d2ea718485b4
Rocketdy91 <87232871+Rocketdy91@users.noreply.github.com>
2022-02-24 10:26:44 -0500

    Added the database

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDTIS67MLX--dfUWIjnNCZw9Z-5wWXxVKc",
+  authDomain: "project-67-e3229.firebaseapp.com",
+  databaseURL: "https://project-67-e3229-default-rtdb.firebaseio.com",
+  projectId: "project-67-e3229",
+  storageBucket: "project-67-e3229.appspot.com",
+  messagingSenderId: "1082435952151",
+  appId: "1:1082435952151:web:d2afd4b36c58eeb676c652"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
