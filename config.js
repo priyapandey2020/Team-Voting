d1aa1954ebd77dea63764835b7628046962c088e
Riothebest <riothebestest04@gmail.com>
2022-02-24 09:15:04 +0530

    version1

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyCOqVQZuuvAVmNZxBxsMx6XXbW1vC2wcLw",
+    authDomain: "c67-project-c0435.firebaseapp.com",
+    databaseURL: "https://c67-project-c0435-default-rtdb.firebaseio.com",
+    projectId: "c67-project-c0435",
+    storageBucket: "c67-project-c0435.appspot.com",
+    messagingSenderId: "196442167286",
+    appId: "1:196442167286:web:8521e467a97049d00bd9d7",
+    measurementId: "G-3L23NSZ47D"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
