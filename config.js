a4d9f861cc826390217ee8554e5c7f0773710621
Aryan Patel <aryanpat2007@gmail.com>
2022-03-06 09:27:39 -0600

    style change and firebase

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBYuae2IIYUTBoAX-BjCYdmGRgTmoPrp5c",
+    authDomain: "team-voting-348b5.firebaseapp.com",
+    projectId: "team-voting-348b5",
+    storageBucket: "team-voting-348b5.appspot.com",
+    messagingSenderId: "188347887818",
+    appId: "1:188347887818:web:bdd3f503a41b1a0d99c433",
+    measurementId: "G-JTRLKH4C87"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
 export default firebase.database();
