66ada6360ba3c3e1a6911c6cfb4827d6de754f54
tywab <86836839+tywab@users.noreply.github.com>
2022-03-08 17:17:04 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyA7THHHo2KtRtQI_P_tX3gIRsefCl5Vo04',
+  authDomain: 'class-test-e351e.firebaseapp.com',
+  databaseURL: 'https://class-test-e351e-default-rtdb.firebaseio.com',
+  projectId: 'class-test-e351e',
+  storageBucket: 'class-test-e351e.appspot.com',
+  messagingSenderId: '630762773807',
+  appId: '1:630762773807:web:09c9857bc6c630e42015da',
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
