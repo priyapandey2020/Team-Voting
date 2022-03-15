7b5646d6c5860e299a599de446f555c598ab62f3
Shaunak7 <Shaunakagarwal2007@hotmail.com>
2022-03-15 15:59:02 +0530

    Modified

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+var firebaseConfig = {apiKey: "AIzaSyDZ2GD2MQ8K3JjgixApkrv_5FlP7cLLnw4",
+    authDomain: "team-voting-app-99da6.firebaseapp.com",
+    databaseURL: "https://team-voting-app-99da6-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-99da6",
+    storageBucket: "team-voting-app-99da6.appspot.com",
+    messagingSenderId: "744565381947",
+    appId: "1:744565381947:web:ddbef2232477d73a17b51e"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
