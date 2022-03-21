b171e5fc8799918333d72100a609e03f9409a068
tishyaDesigns <tishya.karar@gmail.com>
2022-03-21 19:50:25 +0530

    Background change

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCc04Y5xb9eJiRwKT8sxHAHJs-RAvy-kr4",
+    authDomain: "project67-22896.firebaseapp.com",
+    databaseURL: "https://project67-22896-default-rtdb.firebaseio.com",
+    projectId: "project67-22896",
+    storageBucket: "project67-22896.appspot.com",
+    messagingSenderId: "754198767538",
+    appId: "1:754198767538:web:0c8c571e953188187de6ab"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
