e55394b9162217775cdfaafdc303c6699e3fbdc6
unknown <nandiniemaar@gmail.com>
2022-03-25 12:58:24 +0530

    firebase added

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDPx1vpLoDLZuxbzGzMC2LtFvxjAEyGkOc",
+    authDomain: "pro---67.firebaseapp.com",
+    projectId: "pro---67",
+    storageBucket: "pro---67.appspot.com",
+    messagingSenderId: "505303808334",
+    appId: "1:505303808334:web:95a8f21ee4e8041a4f461f"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
