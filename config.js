c9124b1160a1a97490d0522561a3db18efd9051d
coder214 <fathimaneshwa93@gmail.com>
2022-03-27 12:10:20 +0530

    I have done

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCIW1-3JsTyraeoqZeFwpWb8JXLpH_HZ-w",
+    authDomain: "team-voting-f8618.firebaseapp.com",
+    projectId: "team-voting-f8618",
+    storageBucket: "team-voting-f8618.appspot.com",
+    messagingSenderId: "962297295735",
+    appId: "1:962297295735:web:ce030342a6a35ba8372772"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
