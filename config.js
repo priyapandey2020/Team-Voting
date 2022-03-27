7145ef504c63c123a4ac7092b07fa46c8b23acb0
ArtiRathore13 <arti.rathore1391@gmail.com>
2022-03-27 21:04:06 +0530

    Changed db config and font size of team name

--- config.js
+++ config.js
@@ -2,7 +2,15 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    
+        apiKey: "AIzaSyCCMtLO5Rm4J7KpYeahHo0egmyeHKt-R0E",
+        authDomain: "team-voting-f3cbb.firebaseapp.com",
+        databaseURL: "https://team-voting-f3cbb-default-rtdb.firebaseio.com",
+        projectId: "team-voting-f3cbb",
+        storageBucket: "team-voting-f3cbb.appspot.com",
+        messagingSenderId: "838882722416",
+        appId: "1:838882722416:web:32a8b02fc96c57ff422ab5"
+      
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
