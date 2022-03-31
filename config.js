b5c95ab7068ab5fd04bf40f5478cd955a20b578a
AdrikaMondal <adrikamondal@gmail.com>
2022-03-31 17:41:11 +0530

    hello

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
-};
+    apiKey: "AIzaSyDjpufZEmRD7Se5UtY3AAPcPHWHSuUzsDg",
+    authDomain: "project-60-74ee7.firebaseapp.com",
+    databaseURL: "https://project-60-74ee7-default-rtdb.firebaseio.com",
+    projectId: "project-60-74ee7",
+    storageBucket: "project-60-74ee7.appspot.com",
+    messagingSenderId: "196364668265",
+    appId: "1:196364668265:web:e540a5e1e02d8609c541f8"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
