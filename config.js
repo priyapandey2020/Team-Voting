8bf263133bfd42cc54b455e5f1b7a4a006a94bd2
anvi2 <anvitashekhar2807@gmail.com>
2022-04-02 14:52:33 +0530

    added sdk

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyDXzJ4aWjiHpnxreeOxLYAhcfW8DX_haHc",
+        authDomain: "team-voting-app-447c4.firebaseapp.com",
+        databaseURL: "https://team-voting-app-447c4-default-rtdb.firebaseio.com",
+        projectId: "team-voting-app-447c4",
+        storageBucket: "team-voting-app-447c4.appspot.com",
+        messagingSenderId: "252113452481",
+        appId: "1:252113452481:web:90aece007aa77be71e0340"
+      };
+      
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
