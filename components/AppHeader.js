3e12f48913bec6ec8eb35f578e3468a989145c18
coder214 <fathimaneshwa93@gmail.com>
2022-04-06 17:47:30 +0530

    'Done'

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -5,7 +5,7 @@ export default class AppHeader extends React.Component{
   render(){
     return(
       <View style= {styles.textContainer}>
-        <Text style={styles.text}>Team Voting App</Text>
+        <Text style={styles.text}>Team Voting</Text>
       </View>
     );
   }
