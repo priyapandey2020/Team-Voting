0071b626fdef28d3d7b92fbbdf3d2d8c8c037b99
SteveWong2008 <stevealphawong@gmail.com>
2022-04-09 21:49:09 -0700

    modified the UI and increments scores for voting

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-import firebase from 'firebase';
+import { initializeApp } from "firebase/app";
 
-// add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyD45qnr93VW7AMrFO1LdQp5n7dCcYNT0m4",
+    authDomain: "classtest-b7d8b.firebaseapp.com",
+    databaseURL: "https://classtest-b7d8b-default-rtdb.firebaseio.com",
+    projectId: "classtest-b7d8b",
+    storageBucket: "classtest-b7d8b.appspot.com",
+    messagingSenderId: "507501469590",
+    appId: "1:507501469590:web:8d5126a51eb9fcba68c7b7"
+  };
 
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase.database();
+  const app = initializeApp(firebaseConfig);
