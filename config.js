03a0df95fd798a256452c29f0b269196a2333950
ANSHIKATYAGI30 <anshikatyagi3003@gmail.com>
2022-04-14 20:27:47 +0530

    Added the firebase

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDIztuQRjBWBhonCxaZAQ3Fz_b6xiH6kSM",
+    authDomain: "team-voting-app-e0668.firebaseapp.com",
+    projectId: "team-voting-app-e0668",
+    storageBucket: "team-voting-app-e0668.appspot.com",
+    messagingSenderId: "51949294117",
+    appId: "1:51949294117:web:add7cf5874c44e93d725cc"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
