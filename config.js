bdfc0b07d8fc5f95208c805909d8c51c8d6fc57e
Laysha24 <76842004+Laysha24@users.noreply.github.com>
2022-04-17 19:46:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+  apiKey: "AIzaSyA8yhv2rTSMUnFhJ6-U1JOKIxFr4aFTz_w",
+  authDomain: "teamvoting-72d29.firebaseapp.com",
+  projectId: "teamvoting-72d29",
+  storageBucket: "teamvoting-72d29.appspot.com",
+  messagingSenderId: "570268434798",
+  appId: "1:570268434798:web:efb8f06fe6af5db655817f",
+  measurementId: "G-XH2X28NQEQ"
 
 };
 // Initialize Firebase
