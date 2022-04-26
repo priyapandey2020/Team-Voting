428d8d84265bfd3bb5bc4dee155d4c2ce2312576
Arshika0411 <74136650+Arshika0411@users.noreply.github.com>
2022-04-26 12:39:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyC_d09xn--wrrMTqYPsQqZ4VrNwsEOnIH0",
+  authDomain: "team-voting-app-7056e.firebaseapp.com",
+  databaseURL: "https://team-voting-app-7056e-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-7056e",
+  storageBucket: "team-voting-app-7056e.appspot.com",
+  messagingSenderId: "331827127142",
+  appId: "1:331827127142:web:fe7b694213c45863369ea7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
