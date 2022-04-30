25d8c2374ac3400c2cc166620f446e99bded2959
JnnaGamerz123 <90261352+JnnaGamerz123@users.noreply.github.com>
2022-04-30 14:08:05 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAkSuW07nFhotj4Jwm3l353vHvDKZ0v3yU",
+    authDomain: "classtest-c1d83.firebaseapp.com",
+    databaseURL: "https://classtest-c1d83-default-rtdb.firebaseio.com",
+    projectId: "classtest-c1d83",
+    storageBucket: "classtest-c1d83.appspot.com",
+    messagingSenderId: "869498591782",
+    appId: "1:869498591782:web:e19d8aaf7bf362fc0764e7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
