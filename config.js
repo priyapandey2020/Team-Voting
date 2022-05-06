6918b31625536f8086c1e8aedf3e798b58814f6a
aaravgupte <amol.gupte12@gmail.com>
2022-05-06 15:47:30 +0530

    added the database

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAeTieaI-OpNrKGVkXQAANeidnP_qkqP0g",
+    authDomain: "voting-app-43e76.firebaseapp.com",
+    databaseURL: "https://voting-app-43e76-default-rtdb.firebaseio.com",
+    projectId: "voting-app-43e76",
+    storageBucket: "voting-app-43e76.appspot.com",
+    messagingSenderId: "821215932663",
+    appId: "1:821215932663:web:25a0df14dae16a578323f2"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
