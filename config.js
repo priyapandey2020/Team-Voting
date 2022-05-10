9680dd0654ad1d8a686bd15508593b6f3d01ea24
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-05-10 20:16:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase';
-
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDskHwcgoAxOcXzEv7tukvndspeWBO6b3M",
+  authDomain: "classtest1-2606f.firebaseapp.com",
+  projectId: "classtest1-2606f",
+  storageBucket: "classtest1-2606f.appspot.com",
+  messagingSenderId: "158578185771",
+  appId: "1:158578185771:web:670797049f9a1ad1f4bc6e",
+  measurementId: "G-G9RLGYM1BQ"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase.database();
+  // Initialize Firebase
+ const app= firebase.initializeApp(firebaseConfig);
+  export default firebase.database();
