2a5e22b6a0c3aea7b54a07ccdee7ae62b66a92bd
VarunG843 <78062733+VarunG843@users.noreply.github.com>
2022-05-13 18:02:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyCDQJDcK_VT45MSeuqlQLcfFxosyQtiaYI",
+  authDomain: "voting-79bdd.firebaseapp.com",
+  databaseURL: "https://voting-79bdd-default-rtdb.firebaseio.com",
+  projectId: "voting-79bdd",
+  storageBucket: "voting-79bdd.appspot.com",
+  messagingSenderId: "632176915283",
+  appId: "1:632176915283:web:c3a552de6979018bd1e7a6",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
