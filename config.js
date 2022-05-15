e2e8745a25f36bf7cc6cdef4b1e66960b62a98a8
salmarahim <salma.rahim4391@gmail.com>
2022-05-15 09:26:05 +0530

    changed the background color to green

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDBli82Zky4dA7drrpmN93tqQqECslZ4AM",
+    authDomain: "project67-2f025.firebaseapp.com",
+    projectId: "project67-2f025",
+    storageBucket: "project67-2f025.appspot.com",
+    messagingSenderId: "103494167477",
+    appId: "1:103494167477:web:4cff58a6c45a8498fd87fc"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
