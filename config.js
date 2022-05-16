93ff47272383e4e14e724ca9c38bfe8d3472efa4
saumyaparekh12 <84619774+saumyaparekh12@users.noreply.github.com>
2022-05-16 16:47:54 +0530

    Added configuration key

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyADQIcZ9A5YO_Jvip-gCBno1e7jWl6csMs",
+    authDomain: "team-voting-app-dda17.firebaseapp.com",
+    projectId: "team-voting-app-dda17",
+    storageBucket: "team-voting-app-dda17.appspot.com",
+    messagingSenderId: "609740959976",
+    appId: "1:609740959976:web:6985f613490d785609595d"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
