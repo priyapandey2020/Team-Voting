09b2d9b281acda84d2411954bc6748f2ab757acc
PriyanshJeetSingh <85345133+PriyanshJeetSingh@users.noreply.github.com>
2022-05-27 13:19:30 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyDYVp9EsZyTphe23xfIWytyxGY7q5gIkYA",
+        authDomain: "class-quiz-c19a1.firebaseapp.com",
+        projectId: "class-quiz-c19a1",
+        storageBucket: "class-quiz-c19a1.appspot.com",
+        messagingSenderId: "701096752486",
+        appId: "1:701096752486:web:30946e9860d32afa3244a7"
+      };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
