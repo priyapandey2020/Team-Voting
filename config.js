c1f4281e081fa8745da01c9b2f11339f5e93dea6
MoGaMb02 <79096016+MoGaMb02@users.noreply.github.com>
2022-06-07 18:07:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+   apiKey: "AIzaSyB3nzUdExrUBhdUBj69warCKHoUXWv2Mug",
+  authDomain: "team-voting-app-927bc.firebaseapp.com",
+  databaseURL: "https://team-voting-app-927bc-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-927bc",
+  storageBucket: "team-voting-app-927bc.appspot.com",
+  messagingSenderId: "1091048253454",
+  appId: "1:1091048253454:web:2bf871cac81fdaecf9b45f"
 
 };
 // Initialize Firebase
