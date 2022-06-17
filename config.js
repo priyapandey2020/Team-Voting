252b0abd4d0ff476dc7e567a3b3962509f7fde72
SonamKataria <78798845+SonamKataria@users.noreply.github.com>
2022-06-17 12:12:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyDTtgeJLnvbL6ogXtzIt1_u6cZr8rwAyvk",
+  authDomain: "team-voting-app-c2ed4.firebaseapp.com",
+  projectId: "team-voting-app-c2ed4",
+  storageBucket: "team-voting-app-c2ed4.appspot.com",
+  messagingSenderId: "138128679107",
+  appId: "1:138128679107:web:1049b3c0f47b9e51d4c276"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
