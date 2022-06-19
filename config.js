d47b73b53489b843f5b1c627d18f22c44ced7921
AarghCreate <floatingcomet62@gmail.com>
2022-06-19 11:40:35 +0530

    CONFIG

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyA0byjIh2daySYUhlv-iUQcYNcRAOR1MFw",
+  authDomain: "project67-8e6b4.firebaseapp.com",
+  projectId: "project67-8e6b4",
+  storageBucket: "project67-8e6b4.appspot.com",
+  messagingSenderId: "62849005148",
+  appId: "1:62849005148:web:87c2ca44f22c7a04299899"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
