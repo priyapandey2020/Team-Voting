716174f958f5e8964ccdd7739c36c1c3a13465d9
BernardCode <bernard.w.freund@gmail.com>
2022-06-21 15:26:17 -0700

    firebase configuration added

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyCT9zNKnWWUNkxwTyTwul6NOq5hwC-WSFE",
+    authDomain: "contribute-open-source.firebaseapp.com",
+    databaseURL: "https://contribute-open-source-default-rtdb.firebaseio.com",
+    projectId: "contribute-open-source",
+    storageBucket: "contribute-open-source.appspot.com",
+    messagingSenderId: "514901272782",
+    appId: "1:514901272782:web:1446182eba9d54c8bd489f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
