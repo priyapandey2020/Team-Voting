478e4474812422f3c592254c6a174cd487ac3ceb
Nikolas <80078855+MirrorMiru@users.noreply.github.com>
2022-06-24 14:29:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyDvdnQ6LNwOWD9JrMsOhNApsi65IUbbjfM",
+  authDomain: "pro58-64b95.firebaseapp.com",
+  databaseURL: "https://pro58-64b95-default-rtdb.firebaseio.com/",
+  projectId: "pro58-64b95",
+  storageBucket: "pro58-64b95.appspot.com",
+  messagingSenderId: "323267909737",
+  appId: "1:323267909737:web:9bd620fe26b26c9bceaaf8"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
