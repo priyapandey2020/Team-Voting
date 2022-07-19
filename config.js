b51a405bfd358217d9abf39bb19a897cc4d196a3
bananaapple-coder <mohapatra9@gmail.com>
2022-07-19 11:56:28 -0500

    add the db

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyADj5MxwbVQYjthK6kkyYewjCnzMbkCESQ",
+    authDomain: "buzzer-app-39cf8.firebaseapp.com",
+    databaseURL: "https://buzzer-app-39cf8-default-rtdb.firebaseio.com",
+    projectId: "buzzer-app-39cf8",
+    storageBucket: "buzzer-app-39cf8.appspot.com",
+    messagingSenderId: "36404657929",
+    appId: "1:36404657929:web:bbff8482fc0acda8af1dd8"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
