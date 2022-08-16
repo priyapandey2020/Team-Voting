26fab2e44ebf2610bca577a31616218f89b1846f
SkelePine <82421388+SkelePine@users.noreply.github.com>
2022-08-15 18:59:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
-};
+        apiKey: "AIzaSyALm0Bdq7MiYmgWSGpIV9b3BdbucKPFSuQ",
+        authDomain: "teamvotingapp-dd226.firebaseapp.com",
+        projectId: "teamvotingapp-dd226",
+        storageBucket: "teamvotingapp-dd226.appspot.com",
+        messagingSenderId: "3377028818",
+        appId: "1:3377028818:web:e7ddc86cf1552099b3570b"
+      };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
