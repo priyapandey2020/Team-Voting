019b9795e8c2791b581276ceea6b394726fdcc1f
Gargi-1234 <mcharushila@gmail.com>
2022-09-09 13:57:43 +0530

    styling of button

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
-};
+  apiKey: "AIzaSyDqJGcTrWpo4UFvK3haQAk80hO3B1zOHgY",
+  authDomain: "project67--team-voting.firebaseapp.com",
+  databaseURL: "https://project67--team-voting-default-rtdb.firebaseio.com",
+  projectId: "project67--team-voting",
+  storageBucket: "project67--team-voting.appspot.com",
+  messagingSenderId: "707072988104",
+  appId: "1:707072988104:web:8693b7c7e3827005f166fd"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
