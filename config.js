8e5712ea75115e14376151a4fe8797e8cd06f94c
hasinitanmay2 <rsrinivaskumar@gmail.com>
2022-09-21 19:45:36 -0400

    changed styling and added database config

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDonelKOb1E4rARSlQ36xd-BKn1D4RWCqU",
+    authDomain: "classtest-43564.firebaseapp.com",
+    projectId: "classtest-43564",
+    storageBucket: "classtest-43564.appspot.com",
+    messagingSenderId: "472006402684",
+    appId: "1:472006402684:web:3ba67bb00a817ae217dff9"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
