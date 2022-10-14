61505e0fa24b7414fd6f40ec0fab6b214015bf9c
ARSHADJM <arshadmetkari@gmail.com>
2022-10-13 20:55:13 -0700

    Hello World

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyBPsfJeBLqXzZlqH4PAlmvbQprWuvXPses",
+        authDomain: "team-voting-36f4b.firebaseapp.com",
+        databaseURL: "https://team-voting-36f4b-default-rtdb.firebaseio.com",
+        projectId: "team-voting-36f4b",
+        storageBucket: "team-voting-36f4b.appspot.com",
+        messagingSenderId: "702023632754",
+        appId: "1:702023632754:web:7a421d7876e364aceeffeb"
+      };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
