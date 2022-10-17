f7b89f48fb068b56b235427f80c6555269e5af91
whitehatjr <whitehatjr@gmail.com>
2022-10-17 20:16:17 +0530

    Added firebase config

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8JafyVHn0o3LXu6hG7kxmicGm7G5wO2Q",
+    authDomain: "porject-67.firebaseapp.com",
+    databaseURL: "https://porject-67-default-rtdb.firebaseio.com",
+    projectId: "porject-67",
+    storageBucket: "porject-67.appspot.com",
+    messagingSenderId: "778729013168",
+    appId: "1:778729013168:web:741adbebe6af950ad26075",
+    measurementId: "G-6J507L2SHE"
+  };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
