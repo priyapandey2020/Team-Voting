a58cb41db1b5763578f456c91554a9713b464d70
Katie Bui <katie@katies-air.lan>
2022-11-04 20:55:43 -0700

    firebase

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAAJXSorwqdibPh7bmmqSDPsyHX-Ln3ozE",
+    authDomain: "project-67-a6b82.firebaseapp.com",
+    projectId: "project-67-a6b82",
+    storageBucket: "project-67-a6b82.appspot.com",
+    messagingSenderId: "643719285176",
+    appId: "1:643719285176:web:45a4cc3ac2352a52a7a74d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
