8d54eb626c14461f2c2fc82053f882f3f6ad215c
Kashvi <85011895+KashviAggarwal@users.noreply.github.com>
2022-11-07 20:50:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyD8TjPVV6jIEnKgZFcYryAumbt6IDWtD68",
+  authDomain: "teamvoting-6a072.firebaseapp.com",
+  databaseURL: "https://teamvoting-6a072-default-rtdb.firebaseio.com",
+  projectId: "teamvoting-6a072",
+  storageBucket: "teamvoting-6a072.appspot.com",
+  messagingSenderId: "406141662551",
+  appId: "1:406141662551:web:42e7667af2bb983c8bfa12"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
