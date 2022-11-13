10f6b4d36c8a4a4d31cc620bf8a87dd29c0639a6
siddhartha40 <sinha85nitu@gmail.com>
2022-11-13 11:37:02 +0530

    borderRadius added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -70,10 +70,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
-    borderRadius: 15,
+    borderRadius: 20,
     margin:10,
     width: 150,
-    height: 50,
+    height: 150,
   },
   ratingContainer: {
     alignSelf: 'center',
