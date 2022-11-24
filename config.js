031931b26e436fa0bb00dfb04dbc0aa228d11f07
Kapish Sharma <itskapish@gmail.com>
2022-11-24 16:51:30 +0530

    Confguration Done and Added

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAMOVYTLmD3CDzdEi_lhJia8Q_-3ab-aV4",
+    authDomain: "team-voting-app-ae793.firebaseapp.com",
+    projectId: "team-voting-app-ae793",
+    storageBucket: "team-voting-app-ae793.appspot.com",
+    messagingSenderId: "486000955288",
+    appId: "1:486000955288:web:ea476811fb7461889f25ed",
+    measurementId: "G-HG35FDTFPD"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
