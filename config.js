a432b01e5291962a8f9a5dcc8c46d7ad3c1bd3c4
Joshua D <74057975+JoshuaDouza@users.noreply.github.com>
2023-01-03 19:45:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyChCgxytor8lQ8SCZeYuYCwsN3pbls8yVU",
+  authDomain: "pro-c67-73409.firebaseapp.com",
+  projectId: "pro-c67-73409",
+  storageBucket: "pro-c67-73409.appspot.com",
+  messagingSenderId: "447012648620",
+  appId: "1:447012648620:web:e4cb2a5d10bd34856dd109"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
