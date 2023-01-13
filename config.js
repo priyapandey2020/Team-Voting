9138a2ae7e7128ed4a8fcf639c8b5cff08936473
UnagarKaushal <85570568+UnagarKaushal@users.noreply.github.com>
2023-01-13 18:11:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDM2YP9ceFmxSymNmbjQLghrqtMBDMztMw",
+  authDomain: "classtest-be66b.firebaseapp.com",
+  projectId: "classtest-be66b",
+  storageBucket: "classtest-be66b.appspot.com",
+  messagingSenderId: "934586121808",
+  appId: "1:934586121808:web:8fca2ad6508fb5497adb55"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
