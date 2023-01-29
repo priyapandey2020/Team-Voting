3215a0f06253e21a9231cb6b4043dd4e9ff72dab
AyanOP27 <92026205+AyanOP27@users.noreply.github.com>
2023-01-29 15:05:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+  
+    const firebaseConfig = {
+  apiKey: "AIzaSyD_ehAwBUKZwrDxCcgsZUU0F_fWpR9quZA",
+  authDomain: "vote-13a32.firebaseapp.com",
+  projectId: "vote-13a32",
+  storageBucket: "vote-13a32.appspot.com",
+  messagingSenderId: "57877412609",
+  appId: "1:57877412609:web:a313efd39482a5f9943900"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase.database();
+
+
+    
+  
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase.database();
