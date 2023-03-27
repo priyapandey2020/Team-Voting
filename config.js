0d7860a402b5da5e33da988cbb76c8054833698e
arshith787 <82289984+arshith787@users.noreply.github.com>
2023-03-26 22:13:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBYJv6R8a_gAldyKLzSiK_Ovm7y-p-i7lE",
+    authDomain: "pro-67-3f326.firebaseapp.com",
+    projectId: "pro-67-3f326",
+    storageBucket: "pro-67-3f326.appspot.com",
+    messagingSenderId: "1085382553733",
+    appId: "1:1085382553733:web:e5d8c86cdf8ac2f10ef9ec"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 var firebaseConfig = {
 
 };
