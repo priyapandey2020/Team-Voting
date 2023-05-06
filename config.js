aed29a10caf305c9b79a861eb723ac02621e0bb7
Ammoditaa123 <76115143+Ammoditaa123@users.noreply.github.com>
2023-05-06 10:42:05 +0530

    Update config.js
    SDK updated.

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBSdQn83PrSoLurhqc421WvroFv4QXoxIo",
+  authDomain: "classtest-e8928.firebaseapp.com",
+  projectId: "classtest-e8928",
+  storageBucket: "classtest-e8928.appspot.com",
+  messagingSenderId: "454686966639",
+  appId: "1:454686966639:web:902fed210d8820ba8e9e9a"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
