eab513cc37bbdfa42dadb23228ae426e0f8af86a
Soumyarup123 <84226112+Soumyarup123@users.noreply.github.com>
2023-05-22 13:49:28 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyC-tbL4j9qDo6ggN2JJizdOF-T24IUb8o0",
+    authDomain: "pro-c58-b0e27.firebaseapp.com",
+    databaseURL: "https://pro-c58-b0e27-default-rtdb.firebaseio.com",
+    projectId: "pro-c58-b0e27",
+    storageBucket: "pro-c58-b0e27.appspot.com",
+    messagingSenderId: "180128556592",
+    appId: "1:180128556592:web:528f0137aa13b9c2c32ff5"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
