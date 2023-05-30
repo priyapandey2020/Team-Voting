536aa90df96af4b24611ce1e43b8b665211fff95
RishiVerma2008 <91791943+RishiVerma2008@users.noreply.github.com>
2023-05-30 13:46:31 +0530

    Update config.js
    Added firebase SDK 2 config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA5vZNv1DbanrI_TXyUv2AoVPUDinGZu7k",
+  authDomain: "c-58-b9b96.firebaseapp.com",
+  databaseURL: "https://c-58-b9b96-default-rtdb.firebaseio.com",
+  projectId: "c-58-b9b96",
+  storageBucket: "c-58-b9b96.appspot.com",
+  messagingSenderId: "811380392095",
+  appId: "1:811380392095:web:ab41d012f2c58b0703e2aa"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
