cdf1688f7020d9221dfded1967aa3b055c91cd57
harshita <76101819+HELLOChamp@users.noreply.github.com>
2023-06-08 22:14:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyCSt4ND7EY2ZrLSZRpo7-msvifHjqGMwms",
+  authDomain: "project-67-fab74.firebaseapp.com",
+  projectId: "project-67-fab74",
+  storageBucket: "project-67-fab74.appspot.com",
+  messagingSenderId: "357039541085",
+  appId: "1:357039541085:web:a8fd2c64b1bf46264736b4"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
