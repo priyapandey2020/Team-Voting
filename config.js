8995d6ac5565b9a775711ad98009f951f3903dff
AbhinavGupta2008 <83688265+AbhinavGupta2008@users.noreply.github.com>
2023-06-19 10:51:30 +0530

    Update config.js
    Added Database

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDEWm99QiSCgNQ6_P1KGmKKxPSwWFM_dA0",
+  authDomain: "voting-bc731.firebaseapp.com",
+  projectId: "voting-bc731",
+  databaseURL:"https://voting-bc731-default-rtdb.firebaseio.com",
+  storageBucket: "voting-bc731.appspot.com",
+  messagingSenderId: "834328435502",
+  appId: "1:834328435502:web:61e0490b6e2d68f4d9b8bf"
 
 };
 // Initialize Firebase
