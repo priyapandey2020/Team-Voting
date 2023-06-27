e990f5b09f8e9a8e0f955430e742ae83f93d29c0
fizzy332 <74300648+fizzy332@users.noreply.github.com>
2023-06-27 19:50:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCfuBkBE9QracocARMrn_OZKZqDZo_YSvc",
+  authDomain: "classtest1-dd8a9.firebaseapp.com",
+  projectId: "classtest1-dd8a9",
+  storageBucket: "classtest1-dd8a9.appspot.com",
+  messagingSenderId: "1029695224196",
+  appId: "1:1029695224196:web:cb6a2ebab32c1017c885d1",
+  measurementId: "G-R3ZLD744M2"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
