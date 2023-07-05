9c59040fa77a1feb40426ed565af66a5c00a361c
CrystalSnowflake <80325546+CrystalSnowflake@users.noreply.github.com>
2023-07-05 16:54:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5W6-LIf0ckkkrwMevJ098Hx3sWun6AqQ",
+  authDomain: "team-voting-dab1f.firebaseapp.com",
+  databaseURL: "https://team-voting-dab1f-default-rtdb.firebaseio.com",
+  projectId: "team-voting-dab1f",
+  storageBucket: "team-voting-dab1f.appspot.com",
+  messagingSenderId: "341734927053",
+  appId: "1:341734927053:web:bf68232ca0512ed86e9784"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig)
 
 export default firebase.database();
